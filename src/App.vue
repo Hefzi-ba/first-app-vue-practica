b023ae0a6969341684833fdf8e256bfc280fafc7
Hefzi-ba <bamurillo15@gmail.com>
2022-10-04 16:09:09 -0600

    first app

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MiPrimerComponente />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MiPrimerComponente from './components/MiPrimerComponente.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MiPrimerComponente  
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
